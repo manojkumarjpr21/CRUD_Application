2d8ea3bbe5be1ff7860fc59e8f5d13d7d4ad4203
Manoj Kumar <Manoj Kumar@DESKTOP-037IA34>
2022-06-23 16:22:27 +0530

    Add project files.

---
+++ CRUD_Application/Controllers/EmployeeController.cs
@@ -0,0 +1,90 @@
+﻿using CRUD_Application.Data;
+using CRUD_Application.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUD_Application.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly ApplicationContext context;
+
+        public EmployeeController(ApplicationContext context)
+        {
+            this.context = context; 
+        }
+        public IActionResult Index()
+        {
+            var result = context.Employees.ToList();   
+            return View(result);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Employee emp)
+        {
+            if (ModelState.IsValid)
+            {
+                var emp1 = new Employee()
+                {
+                    Name = emp.Name,
+                    Salary = emp.Salary,
+                    City = emp.City,
+                    State = emp.State
+                };
+                context.Employees.Add(emp1);
+                context.SaveChanges();
+                TempData["error"] = "Record Saved!";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["message"] = "Empty Field cann't be Submit";
+                return View(emp);
+            }
+            
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var emp = context.Employees.SingleOrDefault(x => x.Id == id);
+            context.Employees.Remove(emp);
+            context.SaveChanges();
+            TempData["error"] = "Record Deleted!";
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var emp = context.Employees.SingleOrDefault(x => x.Id == id);
+            var result = new Employee()
+            {
+                Name=emp.Name,
+                City=emp.City,
+                Salary=emp.Salary,
+                State = emp.State
+            };
+            return View(result);
+        }
+        [HttpPost]
+        public IActionResult Edit(Employee emp)
+        {
+            var emp1 = new Employee()
+            {
+                Id = emp.Id,
+                Name = emp.Name,
+                Salary = emp.Salary,
+                City = emp.City,
+                State = emp.State
+            };
+            context.Employees.Update(emp1);
+            context.SaveChanges();
+            TempData["error"] = "Record Updated!";
+            return RedirectToAction("Index");
+             
+
+        }
+    }
+}
---
+++ CRUD_Application/Data/ApplicationContext.cs
@@ -0,0 +1,11 @@
+﻿using CRUD_Application.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_Application.Data
+{
+    public class ApplicationContext:DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+        public DbSet<Employee> Employees { get; set; }  
+    }
+}
